f2812e9b45d1fba1787f90bad367f54e7ed72ff1
DivyaBaria-1228 <divyaajitbariya5672@gmail.com>
2024-12-08 19:12:38 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+/*var typed= new typed(".text",{
+    strings: ["Frontend Developer","Web Developer"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:100,
+    loop:true
+});*/
+
+<script>
+        document.addEventListener("DOMContentLoaded", function () {
+            var typed = new Typed(".text", {
+                strings: ["Frontend Developer", "Web Developer"],
+                typeSpeed: 100,
+                backSpeed: 100,
+                backDelay: 100,
+                loop: true
+            });
+        });
+    </script>
+    
